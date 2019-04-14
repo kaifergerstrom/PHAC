466648fd9a68d52ea3a110835809682b0ae403d9
kaifergerstrom <kaifergerstrom@gmail.com>
2019-04-14 16:04:48 -0400

    First commit added basic header more to come

---
+++ home.php
@@ -0,0 +1,20 @@
+
+<?php
+
+include("includes/header.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<link rel="stylesheet" href="css/styles.css">
+		<title>Home</title>
+	</head>
+
+	<body>
+
+	</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,16 @@
+
+<link rel="stylesheet" href="css/header.css">
+
+<header class='header'>
+
+	<div class='header-title'><img src="images/header/logo.png"></div>
+
+	<div class="header-items">
+		<a href="home.php">Home</a>
+		<a href="membership.php">Membership</a>
+		<a href="home.php">Mission</a>
+		<a href="home.php">History</a>
+		<a href="home.php">Archive</a>
+	</div>
+
+</header>
